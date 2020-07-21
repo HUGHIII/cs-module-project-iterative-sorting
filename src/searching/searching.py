f00817d37d5c2de915dce8d7c24e3017dd9ff4b3
Trey Lloyd <58454082+HUGHIII@users.noreply.github.com>
2020-07-21 17:18:52 -0500

    mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -9,6 +11,18 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    arr.sort()
+    top = len(arr)
+    bottom = 0
 
+    while top > bottom:
+        print(len(arr[bottom:top]))
+        middle = (top + bottom)//2
+        if target == arr[middle]:
+            return middle
+        elif target < arr[middle]:
+            top = middle
+        elif target > arr[middle]:
+            bottom = middle
 
     return -1  # not found
